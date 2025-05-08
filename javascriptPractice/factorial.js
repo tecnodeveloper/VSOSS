2401df6cdafb5c3708164dfa1a5bdc3475a9b0cc
Zain Ali <recluzedev@gmail.com>
2025-05-08 10:56:30 +0500

    add factoiral.js

---
+++ javascriptPractice/factorial.js
@@ -0,0 +1,10 @@
+// let num1 = 0;
+// let num2 = 10;
+let factorial = 1;
+
+for(let i = 10; i > 0; --i )
+{
+    factorial = factorial * i;
+    // document.write(factorial);
+    console.log(factorial);
+}
