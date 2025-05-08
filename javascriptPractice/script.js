5f745f4da5ab20446ceeb9e8e275fb24e23d7cf4
tecnodeveloper <recluzedev@gmail.com>
2025-05-08 08:30:35 +0500

    added javascript practice folder

---
+++ javascriptPractice/script.js
@@ -0,0 +1,32 @@
+// Output method are four
+
+// alert("Hello world");
+// console.log("hello");
+// document.write("Hello new user");
+// document.getElementById("demo").innerHTML = "I have been changed";
+
+// "Double quote string";
+// 'Single quote string';
+// `Tild sign string`;
+
+// Primitive data type
+// number(int float char)
+// strings("anyString")
+// Boolean(true false)
+// console.log(typeof n);
+
+// let
+// var
+// const
+
+
+
+// let x = 12;
+// var y = 23;
+// var z = 21.24;
+// var v = "Rehan";
+// var j = "asdfkja";
+// var t = 'string';
+// var b = `Tild sign`;
+// let n = 'a';
+
