897964fdf1cdc3c0c7d6eedc783f93dd51e9ae7e
Zain Ali <recluzedev@gmail.com>
2026-05-14 12:17:47 +0500

    Add initial JavaScript function and HTML structure for Drum Kit project

---
+++ complete-javascript-course-master/practice/Day1/script.js
@@ -0,0 +1,8 @@
+function add(...number) {
+  let sum = 0;
+  sum = sum + number;
+  return sum;
+}
+
+console.log(add(12, 12));
+console.log(add(1, 1));
---
+++ cpp_oldcode/message.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+    MessageBox(NULL, "Hello world", "Alert",MB_OK);
+    return 0;
+}
